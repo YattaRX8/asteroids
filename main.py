a6b3858035b89c9a9c78d1fc04450bf778e3f837
YattaRX8 <LJNassraway@gmail.com>
2025-09-12 22:10:41 -0700

    added ship, asteroids, and movement

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        # limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
